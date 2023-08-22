3b41f50c389ec331847919dd4299ac8f015fa5eb
Pavel Djundik <xPaw@users.noreply.github.com>
2023-08-22 16:26:16 +0300

    Change github releases request

--- static/main.js
+++ static/main.js
@@ -1,37 +1,36 @@
-fetch( 'https://api.github.com/repositories/42366054/releases?per_page=1' )
+fetch( 'https://api.github.com/repositories/42366054/releases/latest' )
 	.then( function( response )
 	{
+		if( !response.ok )
+		{
+			throw new Error( 'Failed to fetch github release' );
+		}
+
 		return response.json();
 	} )
 	.then( function( response )
 	{
-		if( response.length )
+		for( const asset of response.assets )
 		{
-			response = response[ 0 ];
-
-			const date = new Date( response.published_at );
-
-			for( const asset of response.assets )
+			if( asset.name === 'VRF.exe' )
 			{
-				if( asset.name === 'VRF.exe' )
+				document.getElementById( 'js-download' ).href = asset.browser_download_url;
+				document.getElementById( 'js-download-header' ).href = asset.browser_download_url;
+
+				const version = document.querySelector( '.download-version' );
+				version.href = response.html_url;
+
+				let string = `View release notes for v${response.tag_name}`;
+
+				if( window.innerWidth > 500 )
 				{
-					document.getElementById( 'js-download' ).href = asset.browser_download_url;
-					document.getElementById( 'js-download-header' ).href = asset.browser_download_url;
-
-					const version = document.querySelector( '.download-version' );
-					version.href = response.html_url;
-
-					let string = `View release notes for v${response.tag_name}`;
-
-					if( window.innerWidth > 500 )
-					{
-						string += `, released on ${date.toLocaleDateString()}`;
-					}
-
-					version.textContent = string;
-
-					break;
+					const date = new Date( response.published_at );
+					string += `, released on ${date.toLocaleDateString()}`;
 				}
+
+				version.textContent = string;
+
+				break;
 			}
 		}
 	} );

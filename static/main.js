0b11b969762ca54eff940d132d6ac9f1926cc09a
Pavel Djundik <xPaw@users.noreply.github.com>
2025-06-21 21:44:23 +0300

    Tweak design

--- static/main.js
+++ static/main.js
@@ -15,7 +15,6 @@ fetch( 'https://api.github.com/repositories/42366054/releases/latest' )
 			if( asset.name === 'Source2Viewer.exe' )
 			{
 				document.getElementById( 'js-download' ).href = asset.browser_download_url;
-				document.getElementById( 'js-download-header' ).href = asset.browser_download_url;
 
 				const version = document.querySelector( '.download-version' );
 				version.href = response.html_url;
